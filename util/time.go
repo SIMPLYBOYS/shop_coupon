c91844fd4abd750c2882918c295f824a48325b30
SIMPLYBOYS <ferrari828@gmail.com>
2026-03-20 23:09:00 +0800

    fix(util): call time.Now() once to avoid cross-boundary inconsistency (#59)

--- util/time.go
+++ util/time.go
@@ -3,6 +3,6 @@ package util
 import "time"
 
 func GetSpecificTime(hour, minute, second int) time.Time {
-	specificTime := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), hour, minute, second, 0, time.UTC)
-	return specificTime
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day(), hour, minute, second, 0, time.UTC)
 }
